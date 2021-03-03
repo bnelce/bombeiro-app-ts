3f983d326ada9ec3aa6fa52030269168097409da
Italo Barboza <italo_barboza@hotmail.com>
2021-03-03 17:01:49 -0300

    cadastro de um novo

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,89 +1,135 @@
-import React, { useState } from "react";
-import { ActivityIndicator } from "react-native";
+import React, { useRef, useState } from "react";
+import { ScrollView } from "react-native";
 
-import SVGLoader from "../../components/SVGLoader";
-import CustomIcon from "../../components/CustomIcon";
-import Input from "../../components/Input";
-import { useAuth } from "../../contexts/auth";
+import HeaderNav from "../../components/HeaderNav";
+
+import api from '../../services/api';
 
 import {
   Container,
-  LogoContainer,
-  CardsContainer,
-  Cards,
-  Login,
+  Content,
+  Buttons,
+  ShoppingButtonInfo,
+  Title,
+  Subtitle,
+  Article,
+  Separator,
+  Footer,
+  FooterLeftSide,
+  FooterTitle,
+  FooterDescription,
+  FooterButtonContainer,
+  FooterButton,
+  FooterButtonText,
+  FooterRightSide,
+  TextContainer,
   UserInfoRow,
-  Avatar,
-  Initials,
   User,
-  Name,
-  Account,
-  ChangeAccountButton,
-  ChangeAccountText,
-  ChangeAccountButtonContainer,
+  TInput,
+  LabelPassword,
   SignInButtonContainer,
   SignInButton,
-  SignInText,
-  ISafe,
-  Generate,
-  GenerateButton,
-  GenerateText,
-  Interpag,
-  InterpagButtons,
-  Pay,
-  PayButton,
-  PayText,
-  Receive,
-  ReceiveButton,
-  ReceiveText,
-  TInput
+  SignInText
 } from "./styles";
-import { TextInput } from "react-native-gesture-handler";
 
-const SignUp: React.FC = () => {
+interface Props {
+  navigation: void;
+}
+
+interface PromiseResponse {
+  id_user: number;
+  full_name: string;
+  peace_name: string;
+  phone_number: string;
+  email: string;
+}
+
+const SignUp: React.FC<Props> = ({ navigation }) => {
+  const [menuIsExpanded, setMenuIsExpanded] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const { signIn } = useAuth();
+  const [full_name, setCompleteName] = useState<string>();
+  const [peace_name, setPeaceName] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [phone_number, setPhone] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [passwordConfirmation, setPasswordConfirmation] = useState<string>();
+  
+  const scrollViewRef = useRef<ScrollView>(null);
 
-  function handleSignIn(email: string, password: string) {
-    setLoading(true);
-    signIn(email, password);
+  function handleScrollToTop() {
+    scrollViewRef.current?.scrollTo({ y: 0, animated: true });
+  }
+
+  async function handleSignIn(): void {
+    const response: PromiseResponse = await api.post('/users', { full_name, peace_name, phone_number, email, password});
+    navigation.navigate('SignIn');
   }
 
   return (
-    <Container>
-      <LogoContainer>
-        <SVGLoader name="inter_logo" width={110} height={40} />
-      </LogoContainer>
-      <CardsContainer>
-        <Cards>
-          <Login>
+    <Container showsVerticalScrollIndicator={false} ref={scrollViewRef}>
+      <HeaderNav/>
+      <Content>
+          <TextContainer>
+            <ShoppingButtonInfo>
             <UserInfoRow>              
               <User>
-                <Name>Login</Name>
-                <TInput/>                         
+                <TInput
+                onChangeText={text => setCompleteName(text)} 
+                placeholder="Insira o seu nome Completo"/>                         
               </User>
             </UserInfoRow>
             <UserInfoRow>              
               <User>
-                <Name>Senha</Name>
-                <TInput/>          
+                <TInput
+                onChangeText={text => setPeaceName(text)} 
+                placeholder="Insira o seu nome de Paz"/>                         
+              </User>
+            </UserInfoRow>
+            <UserInfoRow>              
+              <User>
+                <TInput
+                onChangeText={text => setEmail(text)} 
+                placeholder="Insira um email válido"/>                         
+              </User>
+            </UserInfoRow>
+            <UserInfoRow>              
+              <User>
+                <TInput
+                onChangeText={text => setPhone(text)} 
+                placeholder="Insira um número de telefone válido"/>                         
+              </User>
+            </UserInfoRow>
+            <UserInfoRow>              
+              <User>
+                <TInput
+                onChangeText={text => setPassword(text)}
+                secureTextEntry={true}
+                placeholder="Insira uma Senha"
+                />
+              </User>
+            </UserInfoRow>
+            <UserInfoRow>              
+              <User>
+                <TInput
+                onChangeText={text => setPasswordConfirmation(text)}
+                secureTextEntry={true}
+                placeholder="Repita a Senha digitada"
+                />
               </User>
             </UserInfoRow>
             <SignInButtonContainer
-              onPress={() => handleSignIn("italo_barboza@hotmail.com", "1234567890")}
             >
               <SignInButton>
-                {loading ? (
-                  <ActivityIndicator size="small" color="#fff" />
-                ) : (
-                  <SignInText>ENTRAR</SignInText>
-                )}
+                  <SignInText
+                  onPress={() => handleSignIn()}
+                  >CADASTRAR</SignInText>
               </SignInButton>
             </SignInButtonContainer>
-          </Login>
-        </Cards>
-      </CardsContainer>
+            </ShoppingButtonInfo>
+          </TextContainer>
+        <Separator />
+        </Content>
     </Container>
   );
 };
